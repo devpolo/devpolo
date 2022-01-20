48994c58de664c9539afc8f6e284c5be85433ff7
DevPolo <paul.crussaire@gmail.com>
2022-01-20 17:11:22 +0100

    feat: init intro section

--- src/modules/home/components/Intro.tsx
+++ src/modules/home/components/Intro.tsx
@@ -1,9 +1,26 @@
 import styled from "styled-components"
 
 const Intro = () => {
-  return <Container>Paul</Container>
+  return (
+    <Container>
+      <h1>Paul Crussaire</h1>
+      <h2>Freelance Developer</h2>
+    </Container>
+  )
 }
 
-const Container = styled.section``
+const Container = styled.section`
+  min-height: 100vh;
+  background: var(--dark);
+
+  h1 {
+    color: var(--white);
+  }
+
+  h2 {
+    color: var(--white);
+    font-weight: normal;
+  }
+`
 
 export default Intro
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,20 +2,15 @@ export const theme = {
   sizes: {},
 
   colors: {
-    black: "rgb(0, 0, 0)",
-    white: "rgb(255, 255, 255)",
+    black: "#000",
+    white: "#fff",
     transparent: "transparent",
 
-    // in used
-    gray50: "rgb(249, 250, 251)",
-    gray100: "rgb(243, 244, 246)",
-    gray200: "rgb(229, 231, 235)",
-    gray300: "rgb(209, 213, 219)",
-    gray400: "rgb(156, 163, 175)",
-    gray500: "rgb(107, 114, 128)",
-    gray600: "rgb(75, 85, 99)",
-    gray700: "rgb(55, 65, 81)",
-    gray800: "rgb(31, 41, 55)",
-    gray900: "rgb(17, 24, 39)",
+    gray: "#4D616E",
+    dark: "#24292E",
+    green: "#56B3B4",
+    yellow: "#F7BA3E",
+    red: "#EA5E5E",
+    purple: "#BF85BF",
   },
 }
