0bd957c0d90a173b7f2a4e5445f6b919bbc8b102
DevPolo <paul.crussaire@gmail.com>
2022-03-25 18:25:55 +0100

    feat: header

--- src/elements/containers/Page.tsx
+++ src/elements/containers/Page.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next"
 
 import Head from "next/head"
+
 import { FULL_NAME } from "~/constants/text"
 
 const isProd = process.env.ENVIRONMENT === "prod"
@@ -13,20 +14,17 @@ interface IPageProps {
 
 const Page: NextPage<IPageProps> = ({
   children,
-  title,
   noindex = false,
-  nofollow = false,
+  ...props
 }) => {
-  const pageTitle = title ? `${FULL_NAME} | ${title}` : FULL_NAME
-  const index = noindex ? "noindex" : "index"
-  const follow = nofollow ? "nofollow" : "follow"
-  const content = isProd ? `${index}, ${follow}` : "noindex, nofollow"
+  const title = props.title ? `${FULL_NAME} | ${props.title}` : FULL_NAME
+  const isIndexed = isProd && !noindex
 
   return (
     <>
       <Head>
-        <title>{pageTitle}</title>
-        <meta name='robots' content={content} />
+        <title>{title}</title>
+        {!isIndexed ? <meta name='robots' content='noindex, nofollow' /> : null}
       </Head>
       {children}
     </>
